82dbcae649588112a39287ca9e2b2e6478c7e538
Luca <luca.freckmann@gmail.com>
2024-05-30 13:34:28 +0200

    updated run_script.sh

--- run_batch.sh
+++ run_batch.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-#SBATCH --partition=grete:shared  # Requesting partition 'grete:shared'
-#SBATCH --gres=gpu:A100:1          # Requesting 1 A100 GPU
-#SBATCH --time=2-00:00:00         # Max wallclock time of 2 days
-#SBATCH --account=nim00007         # Charge the job to account 'nim00007'
-#SBATCH --job-name=mito-net     # Optional: Set a name for the job (replace with your desired name)
+#SBATCH --partition=grete:shared
+#SBATCH --gres=gpu:A100:1
+#SBATCH --time=2-00:00:00
+#SBATCH --account=nim00007
+#SBATCH --job-name=mito-net
+#SBATCH -c 7
+#SBATCH --ntasks=1
 
-srun main.py \
+# source ~/.bashrc
+# conda activate synapse
+
+srun /home/nimlufre/miniforge3/envs/synapse/bin/python /home/nimlufre/synapse/main.py \
+  --experiment_name "mito-net-bs1-ps-64" \
+  --patch_shape 64 512 512 \
+  --batch_size 1
 #  --checkpoint_path /home/nimlufre/synapse \  # use only if weights are to be loaded
-  --experiment_name mito-net-bs1-ps-48 \
-  --patch_shape 48 384 384 \
-  --batch_size 1 \
 
-
+#   --experiment_name mito-net-bs1-ps-48 \
+#   --patch_shape 48 384 384 \
+#   --batch_size 1 \
+# 
