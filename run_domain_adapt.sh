6d03d5df4e8028c8979e8311130b914450e08cfd
Luca <luca.freckmann@uni-goettingen.de>
2024-10-18 08:51:25 +0200

    changes

--- run_domain_adapt.sh
+++ run_domain_adapt.sh
@@ -14,8 +14,10 @@ source /home/nimlufre/.bashrc
 conda activate synapse
 
 python /user/freckmann15/u12103/synapse/train_mito_domain_adaptation.py \
-  --experiment_name "mito-domain-adapt-s2" \
-  --data_dir /mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/wichmann/extracted/endbulb_of_held_s2 \
+  --experiment_name "mito-domain-adapt-s2-sampler" \
+  --data_dir /mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/wichmann/extracted/endbulb_of_held/ \
   --checkpoint_path "/scratch-grete/usr/nimlufre/synapse/mito_segmentation/checkpoints/mitotomo-net32-bs4-ps32256-lr1e-4-downscaled/" #\
   # --n_iterations 100000 \
   # --patch_shape 32 256 256 \
+
+# mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/wichmann/extracted/endbulb_of_held_s2/
