2ae6f91b5ee052edfa50cecc7a66e2629595f2d2
Luca <luca.freckmann@uni-goettingen.de>
2024-12-18 08:56:23 +0100

    added train scripts and segemtnation for wichmann mitos

---
+++ segment_mitos_wichmann.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+from glob import glob
+import torch_em
+import torch_em.transform
+from tqdm import tqdm
+from elf.io import open_file
+import numpy as np
+from synapse_net.inference.mitochondria import segment_mitochondria
+
+# -m /scratch-grete/usr/nimlufre/synapse/mito_segmentation/checkpoints/mito-domain-adapt-s2-sampler-ct5
+
+
+def main(visualize=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_path", "-b",  type=str, default="/scratch-grete/projects/nim00007/data/mitochondria/wichmann/extracted/", help="Path to the root data directory")
+    parser.add_argument("--export_path", "-e",  type=str, default="/scratch-grete/projects/nim00007/data/mitochondria/wichmann/new_mito_labels/", help="Path to the root data directory")
+    parser.add_argument("--model_path", "-m", type=str, default="/scratch-grete/projects/nim00007/models/exports_for_cooper/mito_model_s2.pt")
+    args = parser.parse_args()
+    print(args.base_path)
+
+    h5_paths = sorted(glob(os.path.join(args.base_path, "**", "*.h5"), recursive=True))#, reverse=True)
+
+    print("len(h5_paths)", len(h5_paths))
+    tiling = {"tile": {"z": 32+2*8, "y": 512+128*2, "x": 512+128*2}, "halo": {"z": 8, "y": 128, "x": 128}}
+    scale = 1
+
+    for path in tqdm(h5_paths):
+        with open_file(path, "r") as f:
+            data = f["raw"][:]
+            mean = np.mean(data)
+            # valid_min, valid_max = -5, 5
+            # valid_mask = (data >= valid_min) & (data <= valid_max)
+            # data[~valid_mask] = mean
+            # min_val = np.min(valid_data)
+            # max_val = np.max(valid_data)
+            # data = data[valid_mask] = 2 * (valid_data - min_val) / (max_val - min_val) - 1
+            image = torch_em.transform.raw.standardize(data)
+            print("mean of data and image", mean, np.mean(image))
+            print("max values of data and image", np.max(data), np.max(image))
+        output_path = os.path.join(args.export_path, "ND_scale_" + str(int(scale*10)) + "_" + os.path.basename(path))
+        if os.path.exists(output_path):
+            print("Skipping... output path exists", output_path)
+            continue
+        seg, pred = segment_mitochondria(
+            image, args.model_path,
+            scale=scale,
+            tiling=tiling,
+            return_predictions=True
+            )
+        with open_file(output_path, "w", ".h5") as f1:
+            f1["labels/mitochondria"] = seg
+            f1["pred"] = pred
+            f1["raw"] = data
+            print("Saved to", output_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ train_mitochondria_combined.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=grete:shared
+#SBATCH -G A100:1
+#SBATCH --time=2-00:00:00
+#SBATCH --account=nim00007
+#SBATCH --nodes=1
+#SBATCH -c 12
+#SBATCH --mem 128G
+#SBATCH --job-name=train-mito-net32
+#SBATCH --constraint 80gb
+
+
+source /home/nimlufre/.bashrc
+conda activate synapse
+
+python /user/freckmann15/u12103/synapse/main.py \
+  --experiment_name "mitotomo-net32-bs2-ps32256-lr1e-4-combined" \
+  --data_dir /scratch-grete/projects/nim00007/data/mitochondria/wichmann/extracted/ \
+  --data_dir2 /scratch-grete/projects/nim00007/data/mitochondria/cooper/fidi_down_s2/ \
+  --n_iterations 100000 \
+  --patch_shape 32 256 256 \
+  --batch_size 4 \
+  --learning_rate 1e-4 \
+  --feature_size 32 \
+  --without_rois 1
