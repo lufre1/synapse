512ab7090b466dc6fdbc1a479a2ec2c1353f917b
Luca <luca.freckmann@uni-goettingen.de>
2025-07-29 16:56:56 +0200

    added bash script for sam eval on cellmaps

---
+++ evaluation/run_cellmap_segment_microsam.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=grete:shared
+#SBATCH -G A100:1
+#SBATCH --time=0-12:00:00
+#SBATCH --job-name=cellmap-sam
+#SBATCH -c 8
+#SBATCH --mem 64G
+
+
+source /user/freckmann15/u12103/.bashrc
+micromamba activate /mnt/lustre-grete/usr/u12103/micromamba/envs/sam
+
+# ================ Define ALL parameters here ONCE ================
+BLOCK_SHAPE="1 256 256"
+DD="/mnt/lustre-grete/usr/u12103/cellmap/resized_crops/"
+RAW_KEY="raw"
+LABEL_KEY="label_crop/all"
+WITH_DISTANCES=True
+EXPORT_PATH="/scratch-grete/usr/nimlufre/cellmap/test_segmentations_microsam_resized-new/"
+FORCE_OVERRIDE=True
+MODEL_PATH="/scratch-grete/usr/nimlufre/cellmap/checkpoints/microsam-cellmaps-bs1-ps256-resized-all-new"
+
+
+python /user/freckmann15/u12103/synapse/evaluation/segment_with_microsam.py \
+  --block_shape ${BLOCK_SHAPE} \
+  --base_path ${DD} \
+  --key ${RAW_KEY} \
+  --label_key ${LABEL_KEY} \
+  --with_distances \
+  --export_path ${EXPORT_PATH} \
+  --force_override
