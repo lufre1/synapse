5951294e7b979e3627e82a7e25c2ba18750eae62
GOESTERN-0971714 <u15205@glogin10.usr.hpc.gwdg.de>
2026-05-04 14:57:27 +0200

    changed partition for volume em mito segmentation

--- inference/mitochondria/run_segment_mitos_4010.sh
+++ inference/mitochondria/run_segment_mitos_4010.sh
@@ -4,9 +4,9 @@
 #SBATCH --time=0-5:00:00
 #SBATCH --job-name=inference-volume-em
 #SBATCH -c 8
-#SBATCH --mem 128G
-#SBATCH --partition=grete:shared
-#SBATCH -G A100:1
+#SBATCH --mem 256G
+#SBATCH --partition=standard96s:shared
+## SBATCH -G A100:1
 
 source /mnt/vast-nhr/home/freckmann15/u15205/.bashrc
 micromamba activate /mnt/vast-nhr/home/freckmann15/u15205/micromamba/envs/synapse
